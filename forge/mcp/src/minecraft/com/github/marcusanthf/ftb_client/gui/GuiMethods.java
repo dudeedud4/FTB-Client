7ce5e928ebfc1a2df784de7d01e0be2821a955bd
marcusant <mstojcevich@gmail.com>
2013-02-18 16:10:14 -0500

    Add more GUI methods

--- forge/mcp/src/minecraft/com/github/marcusanthf/ftb_client/gui/GuiMethods.java
+++ forge/mcp/src/minecraft/com/github/marcusanthf/ftb_client/gui/GuiMethods.java
@@ -198,55 +198,29 @@ public class GuiMethods {
 		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 
-	public static void drawVerticalLine(int x, int y, int y2, float l1, int col1) {
-		float f = (float)(col1 >> 24 & 0xFF) / 255F;
-		float f1 = (float)(col1 >> 16 & 0xFF) / 255F;
-		float f2 = (float)(col1 >> 8 & 0xFF) / 255F;
-		float f3 = (float)(col1 & 0xFF) / 255F;
-
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glEnable(GL11.GL_LINE_SMOOTH);
-
-		GL11.glPushMatrix();
-		GL11.glColor4f(f1, f2, f3, f);
-		GL11.glLineWidth(l1);
-		GL11.glBegin(GL11.GL_LINES);
-		GL11.glVertex2d(x, y);
-		GL11.glVertex2d(x, y2);
-		GL11.glEnd();
-		GL11.glPopMatrix();
-
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glDisable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_LINE_SMOOTH);
-	}
-
-	public static void drawHorizontalLine(int x, int x2, int y, float l1, int col1) {
-		float f = (float)(col1 >> 24 & 0xFF) / 255F;
-		float f1 = (float)(col1 >> 16 & 0xFF) / 255F;
-		float f2 = (float)(col1 >> 8 & 0xFF) / 255F;
-		float f3 = (float)(col1 & 0xFF) / 255F;
-
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glEnable(GL11.GL_LINE_SMOOTH);
-
-		GL11.glPushMatrix();
-		GL11.glColor4f(f1, f2, f3, f);
-		GL11.glLineWidth(l1);
-		GL11.glBegin(GL11.GL_LINES);
-		GL11.glVertex2d(x, y);
-		GL11.glVertex2d(x2, y);
-		GL11.glEnd();
-		GL11.glPopMatrix();
-
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glDisable(GL11.GL_BLEND);
-		GL11.glDisable(GL11.GL_LINE_SMOOTH);
-	}
+	protected static void drawHorizontalLine(int par1, int par2, int par3, int par4)
+    {
+        if (par2 < par1)
+        {
+            int i = par1;
+            par1 = par2;
+            par2 = i;
+        }
+ 
+        drawRect(par1, par3, par2 + 1, par3 + 1, par4);
+    }
+ 
+    protected static void drawVerticalLine(int par1, int par2, int par3, int par4)
+    {
+        if (par3 < par2)
+        {
+            int i = par2;
+            par2 = par3;
+            par3 = i;
+        }
+ 
+        drawRect(par1, par2 + 1, par1 + 1, par3, par4);
+    }
 
 	public static void drawDiagonalLine(int x, int x2, int y, float l1, int col1) {
 		float f = (float)(col1 >> 24 & 0xFF) / 255F;
@@ -272,4 +246,64 @@ public class GuiMethods {
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
+	
+	public static void drawRoundedRect(int x, int y, int x1, int y1, int size, int borderC, int insideC) {
+        drawVerticalLine(x, y, y1 -1, borderC);
+        drawVerticalLine(x1 - 1, y, y1 - 1, borderC);
+        drawHorizontalLine(x + 1, x1 - 2, y, borderC);
+        drawHorizontalLine(x + 1, x1 - 2, y1 -1, borderC);
+        drawRect(x + size, y + size, x1 - size, y1 - size, insideC);
+          }
+	
+	public static void drawFilledCircle(int i, int j, double d, int k)
+    {
+        float f = (float)(k >> 24 & 0xff) / 255F;
+        float f1 = (float)(k >> 16 & 0xff) / 255F;
+        float f2 = (float)(k >> 8 & 0xff) / 255F;
+        float f3 = (float)(k & 0xff) / 255F;
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(f1, f2, f3, f);
+        GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+ 
+        for (int l = 0; l <= 360; l++)
+        {
+            double d1 = Math.sin(((double)l * Math.PI) / 180D) * d;
+            double d2 = Math.cos(((double)l * Math.PI) / 180D) * d;
+            GL11.glVertex2d((double)i + d1, (double)j + d2);
+        }
+ 
+        GL11.glEnd();
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+ 
+    public static void drawCircle(int i, int j, double d, int k)
+    {
+        float f = (float)(k >> 24 & 0xff) / 255F;
+        float f1 = (float)(k >> 16 & 0xff) / 255F;
+        float f2 = (float)(k >> 8 & 0xff) / 255F;
+        float f3 = (float)(k & 0xff) / 255F;
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(f1, f2, f3, f);
+        GL11.glBegin(GL11.GL_LINE_LOOP);
+ 
+        for (int l = 0; l <= 360; l++)
+        {
+            double d1 = Math.sin(((double)l * Math.PI) / 180D) * d;
+            double d2 = Math.cos(((double)l * Math.PI) / 180D) * d;
+            GL11.glVertex2d((double)i + d1, (double)j + d2);
+        }
+ 
+        GL11.glEnd();
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
 }
