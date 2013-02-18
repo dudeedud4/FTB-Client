ba234f8cb86be719bd9fcee46b27ec370c7ef8ec
marcusant <mstojcevich@gmail.com>
2013-02-18 16:09:02 -0500

    Enable godmode when in freecam

--- forge/mcp/src/minecraft/com/github/marcusanthf/ftb_client/Freecam.java
+++ forge/mcp/src/minecraft/com/github/marcusanthf/ftb_client/Freecam.java
@@ -25,6 +25,7 @@ public class Freecam extends Base {
         this.oldyaw = mc.thePlayer.rotationYaw;
         this.prevfly = Base.fly.hackToggle;
         Base.fly.hackToggle = true;
+        mc.thePlayer.isDead = true;
         mc.thePlayer.noClip = true;
 	}
 	
@@ -33,6 +34,7 @@ public class Freecam extends Base {
 		mc.thePlayer.setPositionAndRotation(this.oldposx, this.oldposy, this.oldposz, this.oldyaw, this.oldpitch);
 		mc.thePlayer.noClip = false;
 		Base.fly.hackToggle = this.prevfly;
+        mc.thePlayer.isDead = false;
 		this.inprogress = false;
 	}
 	
