cd9041d1dc18830f038e0029da457e1ba124632f
Marcusant <doitant@gmail.com>
2013-03-11 20:13:26 -0400

    Save keybinds to a file -- this is really ugly, going to revise later

--- forge/mcp/src/minecraft/com/github/marcusanthf/ftb_client/SetKey.java
+++ forge/mcp/src/minecraft/com/github/marcusanthf/ftb_client/SetKey.java
@@ -1,148 +1,126 @@
 package com.github.marcusanthf.ftb_client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.OutputStreamWriter;
+
 import org.lwjgl.input.Keyboard;
 
 public class SetKey extends Base {
 	private int newKey;
+	
+	File keys = new File(getWorkingDirectory(), "yoloKeys.txt");
 
 	public SetKey(String hackName, String hackCommand, int hackKey) {
 		super(hackName, hackCommand, hackKey);
 		// TODO Auto-generated constructor stub
+		if(getWorkingDirectory().exists()){
+			if(!keys.exists()){
+				try {
+					keys.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					mc.thePlayer.addChatMessage("\2474\247lFailed to create yolokeys.txt");
+					System.out.println("[ERROR] Failed to create yolokeys.txt");
+				}
+			}
+			if(keys.exists()){
+				this.loadKeys();
+			}
+		}
+	}
+	
+	private void loadKeys(){
+        try {
+		if(!keys.exists()){
+			return;
+		}
+			BufferedReader var1 = new BufferedReader(new FileReader(this.keys));
+			String var2 = "";
+			try {
+				while ((var2 = var1.readLine()) != null && var1.readLine() != "" && var1.readLine() != "\n")
+				{
+					try
+					{
+						String[] var3 = var2.split(":");
+						newKey =  Keyboard.getKeyIndex(var3[1].toUpperCase());
+						String hackName = var3[0];
+						System.out.println(hackName + ":" + newKey);
+						for (int i=0; i< hackArray.size(); i++) {
+							Base hack1 = hackArray.get(i);
+							if(hack1.getName().equalsIgnoreCase(hackName) && newKey > -1){
+								hack1.key = newKey;
+							}
+						}
+						
+					}catch(Error e){
+						e.printStackTrace();
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private int getPlatform() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		if (osName.contains("linux"))
+			return 1;
+		if (osName.contains("unix"))
+			return 1;
+		if (osName.contains("solaris"))
+			return 2;
+		if (osName.contains("sunos"))
+			return 2;
+		if (osName.contains("win"))
+			return 3;
+		if (osName.contains("mac"))
+			return 4;
+		return 5;
+	}
+	
+	private File getWorkingDirectory() {
+		String userHome = System.getProperty("user.home", ".");
+		File workingDirectory;
+		switch (getPlatform()) {
+		case 1:
+		case 2:
+			workingDirectory = new File(userHome, ".minecraft/");
+			break;
+		case 3:
+			String applicationData = System.getenv("APPDATA");
+			if (applicationData != null)
+				workingDirectory = new File(applicationData, ".minecraft/");
+			else
+				workingDirectory = new File(userHome, ".minecraft/");
+			break;
+		case 4:
+			workingDirectory = new File(userHome,
+					"Library/Application Support/minecraft");
+			break;
+		default:
+			workingDirectory = new File(userHome, ".minecraft/");
+		}
+		return workingDirectory;
 	}
 
 	public void setKey(String setkey){
-		if(setkey.equalsIgnoreCase("`") || setkey.equalsIgnoreCase("~")){
-			newKey = Keyboard.KEY_GRAVE;
-		}
-		if(setkey.equalsIgnoreCase("1")){
-			newKey = Keyboard.KEY_1;
-		}
-		if(setkey.equalsIgnoreCase("2")){
-			newKey = Keyboard.KEY_2;
-		}
-		if(setkey.equalsIgnoreCase("3")){
-			newKey = Keyboard.KEY_3;
-		}
-		if(setkey.equalsIgnoreCase("4")){
-			newKey = Keyboard.KEY_4;
-		}
-		if(setkey.equalsIgnoreCase("5")){
-			newKey = Keyboard.KEY_5;
-		}
-		if(setkey.equalsIgnoreCase("6")){
-			newKey = Keyboard.KEY_6;
-		}
-		if(setkey.equalsIgnoreCase("7")){
-			newKey = Keyboard.KEY_7;
-		}
-		if(setkey.equalsIgnoreCase("8")){
-			newKey = Keyboard.KEY_8;
-		}
-		if(setkey.equalsIgnoreCase("9")){
-			newKey = Keyboard.KEY_9;
-		}
-		if(setkey.equalsIgnoreCase("0")){
-			newKey = Keyboard.KEY_0;
-		}
-		if(setkey.equalsIgnoreCase("q")){
-			newKey = Keyboard.KEY_Q;
-		}
-		if(setkey.equalsIgnoreCase("w")){
-			newKey = Keyboard.KEY_W;
-		}
-		if(setkey.equalsIgnoreCase("e")){
-			newKey = Keyboard.KEY_E;
-		}
-		if(setkey.equalsIgnoreCase("r")){
-			newKey = Keyboard.KEY_R;
-		}
-		if(setkey.equalsIgnoreCase("t")){
-			newKey = Keyboard.KEY_T;
-		}
-		if(setkey.equalsIgnoreCase("y")){
-			newKey = Keyboard.KEY_Y;
-		}
-		if(setkey.equalsIgnoreCase("u")){
-			newKey = Keyboard.KEY_U;
-		}
-		if(setkey.equalsIgnoreCase("i")){
-			newKey = Keyboard.KEY_I;
-		}
-		if(setkey.equalsIgnoreCase("O")){
-			newKey = Keyboard.KEY_O;
-		}
-		if(setkey.equalsIgnoreCase("p")){
-			newKey = Keyboard.KEY_P;
-		}
-		if(setkey.equalsIgnoreCase("[") || setkey.equalsIgnoreCase("{")){
-			newKey = Keyboard.KEY_LBRACKET;
-		}
-		if(setkey.equalsIgnoreCase("]") || setkey.equalsIgnoreCase("}")){
-			newKey = Keyboard.KEY_RBRACKET;
-		}
-		if(setkey.equalsIgnoreCase("a")){
-			newKey = Keyboard.KEY_A;
-		}
-		if(setkey.equalsIgnoreCase("s")){
-			newKey = Keyboard.KEY_S;
-		}
-		if(setkey.equalsIgnoreCase("d")){
-			newKey = Keyboard.KEY_D;
-		}
-		if(setkey.equalsIgnoreCase("f")){
-			newKey = Keyboard.KEY_F;
-		}
-		if(setkey.equalsIgnoreCase("g")){
-			newKey = Keyboard.KEY_G;
-		}
-		if(setkey.equalsIgnoreCase("h")){
-			newKey = Keyboard.KEY_H;
-		}
-		if(setkey.equalsIgnoreCase("j")){
-			newKey = Keyboard.KEY_J;
-		}
-		if(setkey.equalsIgnoreCase("k")){
-			newKey = Keyboard.KEY_K;
-		}
-		if(setkey.equalsIgnoreCase("l")){
-			newKey = Keyboard.KEY_L;
-		}
-		if(setkey.equalsIgnoreCase(";") || setkey.equalsIgnoreCase(":")){
-			newKey = Keyboard.KEY_SEMICOLON;
-		}
-		if(setkey.equalsIgnoreCase("'") || setkey.equalsIgnoreCase("\"")){
-			newKey = Keyboard.KEY_APOSTROPHE;
-		}
-		if(setkey.equalsIgnoreCase("z")){
-			newKey = Keyboard.KEY_Z;
-		}
-		if(setkey.equalsIgnoreCase("x")){
-			newKey = Keyboard.KEY_X;
-		}
-		if(setkey.equalsIgnoreCase("c")){
-			newKey = Keyboard.KEY_C;
-		}
-		if(setkey.equalsIgnoreCase("v")){
-			newKey = Keyboard.KEY_V;
-		}
-		if(setkey.equalsIgnoreCase("b")){
-			newKey = Keyboard.KEY_B;
-		}
-		if(setkey.equalsIgnoreCase("n")){
-			newKey = Keyboard.KEY_N;
-		}
-		if(setkey.equalsIgnoreCase("m")){
-			newKey = Keyboard.KEY_M;
-		}
-		if(setkey.equalsIgnoreCase(",") || setkey.equalsIgnoreCase("<")){
-			newKey = Keyboard.KEY_COMMA;
-		}
-		if(setkey.equalsIgnoreCase(".") || setkey.equalsIgnoreCase(">")){
-			newKey = Keyboard.KEY_PERIOD;
-		}
-		if(setkey.equalsIgnoreCase("/") || setkey.equalsIgnoreCase("?")){
-			newKey = Keyboard.KEY_SLASH;
-		}
+		newKey = Keyboard.getKeyIndex(setkey.toUpperCase());
 	}
 	
 	@Override
@@ -156,21 +134,69 @@ public class SetKey extends Base {
 					Base hack1 = hackArray.get(i);
 					if(hack1.getName().equalsIgnoreCase(as1[2]) && newKey > -1){
 						hack1.key = newKey;
+						OutputStreamWriter writer = new OutputStreamWriter( new FileOutputStream(this.keys, true), "UTF-8");
+						BufferedWriter bufferedWriter = new BufferedWriter(writer);
+						bufferedWriter.write(hack1.getName() + ":" + as1[3]);
+						bufferedWriter.newLine();
+						bufferedWriter.close();
 					}
 				}
 				}else if(as1[1].equalsIgnoreCase("del") || as1[1].equalsIgnoreCase("rm") || as1[1].equalsIgnoreCase("delete")){
 					for (int i=0; i< hackArray.size(); i++) {
 						Base hack1 = hackArray.get(i);
 						if(hack1.getName().equalsIgnoreCase(as1[2]) && newKey > -1){
-							hack1.key = 0;
+							hack1.key = -1;
 						}
+						try {
+							if(!keys.exists()){
+								return;
+							}
+								BufferedReader var1 = new BufferedReader(new FileReader(this.keys));
+								String var2 = "";
+								try {
+									while ((var2 = var1.readLine()) != null)
+									{
+										try
+										{
+											String[] var3 = var2.split(":");
+											newKey =  Keyboard.getKeyIndex(var3[1].toUpperCase());
+											String hackName = var3[0];
+											System.out.println(hackName + ":" + newKey);
+											for (int i1=0; i1< hackArray.size(); i1++) {
+												Base hack11 = hackArray.get(i1);
+												if(hack11.getName().equalsIgnoreCase(hackName)){
+													if((hack11.getName().equalsIgnoreCase(as1[2]) || hack11.getCommand().equalsIgnoreCase(as1[2]))){
+														FileWriter fileWriter = new FileWriter(this.keys);
+														BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+														bufferedWriter.write(hack11.getName() + ":" + "-1");
+														bufferedWriter.newLine();
+														bufferedWriter.close();
+														hack1.key = -1;
+														hack11.key = -1;
+													}
+												}
+											}
+											
+										}catch(Error e){
+											e.printStackTrace();
+										}
+									}
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+								
+								
+							} catch (FileNotFoundException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 				}
 				}else if(as1[1].equalsIgnoreCase("help") || as1[1].equalsIgnoreCase(null)){
 					mc.thePlayer.addChatMessage("\247bUsage: \247f.key \2470[\247aadd/del\2470] \247ahackname key");
 					mc.thePlayer.addChatMessage("\247bExample: \247f.key \247aadd \247afly v");
 				}
 			}catch(Exception e){
-				mc.thePlayer.addChatMessage("\2474\247lError!");
 			}
 		}
 	}
